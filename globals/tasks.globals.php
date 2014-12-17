c04262f0850f6c41375c095028218374f3a2d4c9
Jason Varga <jason@pixelfear.com>
2014-12-17 15:25:37 -0500

    Cater for modified site roots

--- globals/tasks.globals.php
+++ globals/tasks.globals.php
@@ -4,7 +4,7 @@ class Tasks_globals extends Tasks
 
 	public function getContent()
 	{
-		$url = $this->fetchConfig('globals_url', null, null, false, false);
+		$url = URL::assemble(Config::getSiteRoot(), $this->fetchConfig('globals_url', null, null, false, false));
 		$content = ContentService::getContentByUrl($url);
 		$content = current($content->extract());
 
